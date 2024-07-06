e1c7bb8654bc47674f70f4a5445fffdc8fe12077
Tarun Gupta <tarunthegreat2003@gmail.com>
2024-07-06 16:13:18 +0530

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'
 import Bg from './Components/Bg.js'
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <div>
+      <Analytics />
       <Bg/>
     </div>
   )
